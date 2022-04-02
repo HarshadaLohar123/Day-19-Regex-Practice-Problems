520f596c4ad1e62ce7a4e10e7fe2d69128e8f860
harshadalohar123 <harshalohar123@gmail.com>
2022-04-02 17:34:55 +0530

    ADDED:User First Name

---
+++ Regex_Problems/Regex_Problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your First name");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][a-z]{2,}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("your First name is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your First name is " + false);
+                    Console.WriteLine("First Name Should Contain Only Four Character");
+                }
+            }
+        }
+    }
+}
